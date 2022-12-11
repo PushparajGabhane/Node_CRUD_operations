948849a8c4e5d8f4a2b94e78f74d40368ad4e3fe
Pushparaj Gabhane <gabhanepushparaj@gmail.com>
2022-12-11 13:04:47 +0530

    all file added

---
+++ src/index.js
@@ -0,0 +1,39 @@
+const fs = require('fs/promises');
+
+const myFileWriter = async (fileName, fileContent) => {
+	// write code here
+	// dont chnage function name
+	fs.writeFile(fileName, fileContent);
+}
+
+const myFileReader = async (fileName) => {
+	// write code here
+	// dont chnage function name
+	fs.readFile(fileName)
+		.then(function (result) {
+			console.log("" + result);
+		})
+		.catch(function (error) {
+			console.log(error);
+		});
+}
+
+
+const myFileUpdater = async (fileName, fileContent) => {
+	// write code here
+	// dont change function name
+	fs.appendFile(fileName, fileContent);
+}
+
+const myFileDeleter = async (fileName) => {
+	// write code here
+	// dont chnage function name
+	fs.unlink(fileName);
+}
+
+//myFileWriter("index.txt","Hello world");
+//myFileReader("index.txt");
+//myFileUpdater("index.txt","\n I this second add");
+myFileDeleter("index.txt");
+
+module.exports = { myFileWriter, myFileUpdater, myFileReader, myFileDeleter }
